83761adac1dd49a9d4167be1c970210d4d6c7c2e
mikhail <aminovmikl.01@gmail.com>
2023-01-18 23:41:03 +0300

    init xposed module

---
+++ app/src/main/java/ru/n00byara/vkadsblock/xposed/Xposed.kt
@@ -0,0 +1,10 @@
+package ru.n00byara.vkadsblock.xposed
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class Xposed : IXposedHookLoadPackage {
+    override fun handleLoadPackage(p0: XC_LoadPackage.LoadPackageParam?) {
+        TODO("Not yet implemented")
+    }
+}
